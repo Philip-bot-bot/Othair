af5c2cad333d23b248b40fdb7d66448806920077
Philly 80 <82407767+Philip-bot-bot@users.noreply.github.com>
2024-07-23 10:04:19 +0000

    PAGE

--- src/App/Settings/index.js
+++ src/App/Settings/index.js
@@ -1,8 +1,11 @@
 import React from "react"
 import Welcome from "./Welcomemessage"
 import ConfirmButton from "./ConfirmButton"
-
+import Page from "../Shared/Page";
 
 export default function () {
-    return <div><Welcome/> <ConfirmButton/></div> 
+    return <Page name="SETTINGS"> 
+        <Welcome/> 
+        <ConfirmButton/>
+        </Page> 
 }
